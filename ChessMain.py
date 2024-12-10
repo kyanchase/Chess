ceb8ab1f240879c44c247e10638a1fa3bea3ed3f
kyanchase <165103120+kyanchase@users.noreply.github.com>
2024-12-09 21:25:58 -0500

    t
    b

---
+++ ChessMain.py
@@ -0,0 +1,13 @@
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8 #dimensions of a chess board are 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+# Initialize a global dictionary of images. This will be called exactly once in the main
+def loadImages():
+    pieces = ['wP', 'wR', 'wN', 'wB', 'wK', 'wK', 'wQ', 'bP', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    IMAGES[piece] = p.image.load("images/")
